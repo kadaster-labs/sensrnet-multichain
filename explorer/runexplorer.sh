7ea37958d558abc18b1560b4766066f90b7e3963
Florijn <wim.florijn@kadaster.nl>
2020-07-27 16:52:43 +0200

    finally working explorer

--- explorer/runexplorer.sh
+++ explorer/runexplorer.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -x
 
-echo "Sleep for 15 seconds so the master node has initialised"
-sleep 5
+echo "Sleep for 10 seconds so the master node has initialised"
+sleep 10
 
 echo "Start the chain"
 multichaind -daemon -txindex -shrinkdebugfilesize $CHAINNAME@$MASTER_NODE_HOST:$MASTER_NODE_PORT -rpcuser=$RPC_USER -rpcpassword=$RPC_PASSWORD -rpcallowip=$RPC_ALLOW_IP -rpcport=$RPC_PORT
 
-echo "Sleep for 15 seconds so the slave node has initialised"
-sleep 5
+echo "Sleep for 10 seconds so the slave node has initialised"
+sleep 10
 
 echo "Setup /root/.multichain/$CHAINNAME/multichain.conf"
 cat << EOF > /root/.multichain/$CHAINNAME/multichain.conf
@@ -16,13 +16,14 @@ rpcpassword=$RPC_PASSWORD
 rpcallowip=$RPC_ALLOW_IP
 rpcport=$RPC_PORT
 EOF
+
 echo "Setup /root/explorer.conf"
 
 cat << EOF > /root/explorer.conf
 port 2750
 host 0.0.0.0
 datadir += [{
-        "dirname": "~/.multichain/$CHAINNAME",
+        "dirname": "/root/.multichain/$CHAINNAME",
         "loader": "default",
         "chain": "MultiChain $CHAINNAME",
         "policy": "MultiChain"
